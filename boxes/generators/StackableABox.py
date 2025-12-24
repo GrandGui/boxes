28ade54c56728add8b409cad2d11a4948ce2fe7f
GandGui <rey.guillaume@gmail.com>
2025-12-24 18:01:18 +0100

    Update generator with the new edge

--- boxes/generators/StackableABox.py
+++ boxes/generators/StackableABox.py
@@ -14,27 +14,22 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from boxes import *
-from boxes.lids import LidSettings
-
 
 class StackableABox(Boxes):
     """An improvement of simple Box with stackable and modular grooves."""
 
-    description = "This box is kept as simple as ABox with stackable edge in every direction. The grooves are repeated to create a modular pattern"
+    description = "This box is kept as simple as ABox with grooves in every direction. The grooves are repeated to create a modular pattern"
 
     ui_group = "Box"
     ui_name = "Stackable A Box"
-    print("GENERATOR CALLED")
+
+    print("Stackable generator called")
 
     def __init__(self) -> None:
         Boxes.__init__(self)
         self.addSettingsArgs(edges.FingerJointSettings)
-        self.addSettingsArgs(edges.GroovedSettings)
-        self.buildArgParser("outside")
-        self.argparser.add_argument(
-            "--M",action="store",type=float,default=50,
-            help="Module (base measure of length, width and height in mm)",
-        )
+        self.addSettingsArgs(edges.ModGroovedSettings)
+
         self.argparser.add_argument(
             "--M_x",action="store",type=int,default=1,
             help="Number of module in x dimension"
@@ -50,52 +45,50 @@ class StackableABox(Boxes):
         self.argparser.add_argument(
             "--top_edge",
             action="store",
-            type=ArgparseEdgeType("FhsezZ"),
-            choices=list("FhsezZ"),
-            default="z",
+            type=ArgparseEdgeType("FhseaA"),
+            choices=list("FhseaA"),
+            default="a",
             help="edge type for top edge",
         )
 
         self.argparser.add_argument(
             "--bottom_edge",
             action="store",
-            type=ArgparseEdgeType("FhsezZ"),
-            choices=list("FhsezZ"),
-            default="Z",
+            type=ArgparseEdgeType("FhseaA"),
+            choices=list("FhseaA"),
+            default="A",
             help="edge type for bottom edge",
         )
 
+        self.buildArgParser("outside")
+
     def render(self):
-        M = self.M
+        M = self.ModGrooved_module
         x, y, h = self.M_x * M, self.M_y * M, self.M_h * M
         t = self.thickness
 
-        print("Render called")
+        top = self.edges.get(self.top_edge, self.edges["e"])
+        bot = self.edges.get(self.bottom_edge, self.edges["F"])
 
-        t1 = t2 = t3 = t4 = self.edges.get(self.top_edge, self.edges["e"])
-        b = self.edges.get(self.bottom_edge, self.edges["F"])
-
-        print(b)
         sideedge = "F" # if self.vertical_edges == "finger joints" else "h"
 
         if self.outside:
             self.x = x = self.adjustSize(x, sideedge, sideedge)
             self.y = y = self.adjustSize(y)
-            self.h = h = self.adjustSize(h, b, t1)
+            self.h = h = self.adjustSize(h, bot, top)
 
         with self.saved_context():
-            self.rectangularWall(x, h, [b, sideedge, t1, sideedge],
+            self.rectangularWall(x, h, [bot, sideedge, top, sideedge],
                                  ignore_widths=[1, 6], move="up",label="Front Wall")
-            self.rectangularWall(x, h, [b, sideedge, t3, sideedge],
+            self.rectangularWall(x, h, [bot, sideedge, top, sideedge],
                                  ignore_widths=[1, 6], move="up",label="Back Wall")
 
             if self.bottom_edge != "e":
                 self.rectangularWall(x, y, "ffff", move="up",label="Bottom Panel")
-            self.lid(x, y)
 
-        self.rectangularWall(x, h, [b, sideedge, t3, sideedge],
+        self.rectangularWall(x, h, [bot, sideedge, top, sideedge],
                              ignore_widths=[1, 6], move="right only",label="Back Wall")
-        self.rectangularWall(y, h, [b, "f", t2, "f"],
+        self.rectangularWall(y, h, [bot, "f", top, "f"],
                              ignore_widths=[1, 6], move="up",label="Left Wall")
-        self.rectangularWall(y, h, [b, "f", t4, "f"],
+        self.rectangularWall(y, h, [bot, "f", top, "f"],
                              ignore_widths=[1, 6], move="up",label="Right Wall")
